548cb257e381177326ad547b779c3fc3d64db074
Christian Despres <despresc@ibm.com>
2023-05-31 13:42:34 +0000

    diagnostic fix

--- runtime/compiler/p/codegen/ForceRecompilationSnippet.cpp
+++ runtime/compiler/p/codegen/ForceRecompilationSnippet.cpp
@@ -51,7 +51,7 @@ uint8_t *TR::PPCForceRecompilationSnippet::emitSnippetBody()
    // |= to update the address chunks. See TR_PPC64RelocationTarget::storeAddressSequence.
    intptr_t startPCRegValue = cg()->canEmitDataForExternallyRelocatableInstructions() ? startPC : 0;
    TR_VerboseLog::writeLineLocked("We're emitting a snippet under %s with startPCRegValue: %lu",
-                                  cg()->compileRelocatableCode() ? "AOT" : "non-AOT",
+                                  cg()->comp()->compileRelocatableCode() ? "AOT" : "non-AOT",
                                   startPCRegValue);
 
    getSnippetLabel()->setCodeLocation(buffer);
