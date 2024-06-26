6a0cafe1302261aea8474750ce00628c10117e0a
Christian Despres <despresc@ibm.com>
2024-06-26 20:04:10 +0000

    unnecessary U_32 null cast

--- runtime/compiler/env/j9methodServer.cpp
+++ runtime/compiler/env/j9methodServer.cpp
@@ -2570,7 +2570,7 @@ TR_ResolvedRelocatableJ9JITServerMethod::staticAttributes(TR::Compilation * comp
 
    if (!resolveField)
       {
-      *address = (U_32)NULL;
+      *address = NULL;
       fieldInfoCanBeUsed = false;
       }
 
