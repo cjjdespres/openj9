96a11f3ba668e1ff127b3c5abe8593129cb0e3db
Christian Despres <despresc@ibm.com>
2024-08-08 14:14:35 -0400

    allow zero vtable slot for archetype specimens

--- runtime/compiler/env/j9method.cpp
+++ runtime/compiler/env/j9method.cpp
@@ -6491,7 +6491,7 @@ TR_ResolvedJ9Method::vTableSlot(U_32 cpIndex)
    {
    TR_ASSERT(cpIndex != -1, "cpIndex shouldn't be -1");
    //UDATA vTableSlot = ((J9RAMVirtualMethodRef *)literals())[cpIndex].methodIndexAndArgCount >> 8;
-   TR_ASSERT(_vTableSlot, "vTableSlot called for unresolved method");
+   TR_ASSERT(isArchetypeSpecimen() || _vTableSlot, "vTableSlot called for unresolved method");
    return _vTableSlot;
    }
 
