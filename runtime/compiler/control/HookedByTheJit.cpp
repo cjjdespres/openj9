60c2a2c30d2646d828e745ba6f1a9698132f93d1
Christian Despres <despresc@ibm.com>
2024-09-27 22:16:21 -0400

    catch a couple of classes

--- runtime/compiler/control/HookedByTheJit.cpp
+++ runtime/compiler/control/HookedByTheJit.cpp
@@ -3736,6 +3736,12 @@ void jitHookClassLoadHelper(J9VMThread *vmThread,
 
    compInfo->getPersistentInfo()->getPersistentClassLoaderTable()->associateClassLoaderWithClass(vmThread, classLoader, clazz);
 
+   // TODO: surely there is a better way of doing this
+   getClassNameIfNecessary(vm, clazz, className, classNameLen);
+   if ((classNameLen == 23 && memcmp(className, "java/lang/J9VMInternals", 23) ||
+       (classNameLen == 17 && memcmp(className, "com/ibm/oti/vm/VM", 17))))
+      compInfo->getPersistentInfo()->getAOTDependencyTable()->onClassLoad(vmThread, clazz);
+
 #if defined(J9VM_OPT_JITSERVER)
    if (auto deserializer = compInfo->getJITServerAOTDeserializer())
       deserializer->onClassLoad(cl, vmThread);
