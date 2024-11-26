b62f86338cb38aa72558b945c5c285c3e8896c89
Christian Despres <despresc@ibm.com>
2024-11-26 13:22:19 -0500

    Help getClassOfStaticFromCP

--- runtime/compiler/runtime/SymbolValidationManager.cpp
+++ runtime/compiler/runtime/SymbolValidationManager.cpp
@@ -1286,6 +1286,10 @@ TR::SymbolValidationManager::validateStaticClassFromCPRecord(uint16_t classID, u
    {
    J9Class *beholder = getJ9ClassFromID(beholderID);
    J9ConstantPool *beholderCP = J9_CP_FROM_CLASS(beholder);
+
+   static bool helpValidateStaticClass = feGetEnv("TR_HelpStaticClassFromCP") != NULL;
+   if (helpValidateStaticClass)
+      TR_ResolvedJ9Method::getClassFromCP(_fej9, beholderCP, _comp, cpIndex);
    return validateSymbol(classID, TR_ResolvedJ9Method::getClassOfStaticFromCP(_fej9, beholderCP, cpIndex));
    }
 
