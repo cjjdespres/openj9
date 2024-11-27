9c86bf441521d4aa0139ddd84d477b65c788eb20
Christian Despres <despresc@ibm.com>
2024-11-27 09:41:05 -0500

    change candidate lookup method

--- runtime/compiler/env/DependencyTable.hpp
+++ runtime/compiler/env/DependencyTable.hpp
@@ -39,7 +39,7 @@ public:
    void classLoadEvent(TR_OpaqueClassBlock *ramClass, bool isClassLoad, bool isClassInitialization) {}
    void invalidateUnloadedClass(TR_OpaqueClassBlock *ramClass) {}
    void invalidateRedefinedClass(TR_PersistentCHTable *table, TR_J9VMBase *fej9, TR_OpaqueClassBlock *oldClass, TR_OpaqueClassBlock *freshClass) {}
-   TR_OpaqueClassBlock *findClassCandidate(uintptr_t offset) { return NULL; }
+   J9Class *findCandidateWithChainAndLoader(TR::Compilation *comp, uintptr_t classChainOffset, void *classLoaderChain) { return NULL; }
    void methodWillBeCompiled(J9Method *method) {}
    void printStats() {}
    };
@@ -118,9 +118,10 @@ public:
    // RAM method of ramClass.
    void invalidateRedefinedClass(TR_PersistentCHTable *table, TR_J9VMBase *fej9, TR_OpaqueClassBlock *oldClass, TR_OpaqueClassBlock *freshClass);
 
-   // Given a ROM class offset, return an initialized class with a valid class
-   // chain starting with that offset.
-   TR_OpaqueClassBlock *findClassCandidate(uintptr_t offset);
+   // Given a class chain and class loader chain, return an initialized class
+   // with a valid class chain starting with that offset and with a class loader
+   // with that loader chain
+   J9Class *findCandidateWithChainAndLoader(TR::Compilation *comp, uintptr_t classChainOffset, void *classLoaderChain);
 
    static uintptr_t decodeDependencyOffset(uintptr_t offset)
       {
