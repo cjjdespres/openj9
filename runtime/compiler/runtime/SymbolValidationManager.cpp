6b46d5521cf2e1965a60ced238672331c83b14af
Christian Despres <despresc@ibm.com>
2024-12-03 17:55:10 -0500

    try new resolution

--- runtime/compiler/runtime/SymbolValidationManager.cpp
+++ runtime/compiler/runtime/SymbolValidationManager.cpp
@@ -1357,7 +1357,9 @@ TR::SymbolValidationManager::validateStaticClassFromCPRecord(uint16_t classID, u
    if (cpIndex != -1)
       {
       TR::VMAccessCriticalSection getClassFromConstantPool(_fej9);
-      _fej9->_vmFunctionTable->resolveClassRef(_fej9->vmThread(), beholderCP, cpIndex, J9_RESOLVE_FLAG_JIT_COMPILE_TIME);
+
+      _fej9->_vmFunctionTable->resolveStaticFieldRef(_fej9->vmThread(), NULL, beholderCP, cpIndex, J9_RESOLVE_FLAG_JIT_COMPILE_TIME, NULL);
+      // _fej9->_vmFunctionTable->resolveClassRef(_fej9->vmThread(), beholderCP, cpIndex, J9_RESOLVE_FLAG_JIT_COMPILE_TIME);
       }
 
    classOfStatic = TR_ResolvedJ9Method::getClassOfStaticFromCP(_fej9, beholderCP, cpIndex);
